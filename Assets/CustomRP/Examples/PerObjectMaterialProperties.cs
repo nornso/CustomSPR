6a8f8ae309d0bfa428bdf23243acaaf7323a8c91
nornso <wusu112358@gmail.com>
2022-04-07 00:56:20 +0800

    Cutoff

--- Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
+++ Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
@@ -6,11 +6,15 @@ using UnityEngine;
 public class PerObjectMaterialProperties : MonoBehaviour
 {
     static int baseColorId = Shader.PropertyToID("_BaseColor");
+    static int cutoffID = Shader.PropertyToID("_Cutoff");
 
     static MaterialPropertyBlock block;
 
     [SerializeField]
     Color baseColor = Color.white;
+
+    [SerializeField, Range(0f, 1f)]
+    float cutoff = 0.5f;
 
     private void Awake()
     {
@@ -23,6 +27,7 @@ public class PerObjectMaterialProperties : MonoBehaviour
             block = new MaterialPropertyBlock();
 
         block.SetColor(baseColorId, baseColor);
+        block.SetFloat(cutoffID, cutoff);
         GetComponent<Renderer>().SetPropertyBlock(block);
     }
 }
